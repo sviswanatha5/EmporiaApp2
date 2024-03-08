901351e80935ec12314a246b3bbf1caacab170eb
sviswanatha6 <sviswanatha6@gatech.edu>
2024-03-08 12:57:35 -0500

    Added everyones stuff

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -36,38 +36,21 @@ class _RegisterScreenState extends State<RegisterScreen> {
     Navigator.pop(context);
 
     try {
-      if (passwordController.text == confirmPasswordController.text) {
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
-            email: emailController.text, password: passwordController.text);
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
 
-        FirebaseAuth user = FirebaseAuth.instance;
-        List<bool> preferences = List.generate(9, (index) => false);
+      FirebaseAuth user = FirebaseAuth.instance;
 
-        FirebaseFirestore.instance
+      FirebaseFirestore.instance
             .collection('users')
             .doc(user.currentUser?.uid)
             .set({
           'uid': user.currentUser?.uid,
           'email': user.currentUser!.email,
-          'preferences': preferences,
-        });
-      } else {
-        //show error message that passwords aren't the same
-        wrongInputMessage("Passwords don't match");
-      }
+        }, SetOptions(merge: true));
+
     } on FirebaseAuthException catch (exception) {
-      //Navigator.pop(context);
-      if (exception.code == 'auth/user-not-found') {
-        //print('No user found/ wrong email');
-
-        wrongInputMessage("Wrong email");
-      } else if (exception.code == 'auth/wrong-password') {
-        //print('Wrong password');
-
-        wrongInputMessage("Wrong password");
-      } else {
-        wrongInputMessage("Wrong email or password");
-      }
+      wrongInputMessage("Wrong email or password");
     }
   }
 
@@ -81,115 +64,97 @@ class _RegisterScreenState extends State<RegisterScreen> {
       },
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-            child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.deepPurple.shade700,
+              Colors.deepPurple.shade500,
+              Colors.deepPurple.shade300,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //FlutterLogo(size: 100), // Temporary placeholder for logo
+                // Make sure your logo is in the assets and properly linked in pubspec.yaml
+                Image.asset('lib/images/logo.jpg', width: 150, height: 150),
 
-              //logo
+                SizedBox(height: 24),
 
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-
-              const SizedBox(height: 50),
-
-              const Text(
-                'Let\'s create an account',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              //username
-
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  obscureText: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration( // Add some styling to input decoration
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: 'Email',
+                Text(
+                  'Welcome!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+
+                Text(
+                  'Let\'s create an account',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                // Email Input Field
+                _buildInputField(
+                  icon: Icons.email,
+                  hintText: 'Email',
+                  controller: emailController,
+                  obscureText: false,
                 ),
 
               //password
 
               const SizedBox(height: 10),
 
-                MyTextField(
-                  controller: passwordController,
+                _buildInputField(
+                  icon: Icons.lock,
                   hintText: 'Password',
+                  controller: passwordController,
                   obscureText: true,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration( // Add some styling to input decoration
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: 'Password',
-                  ),
                 ),
 
               const SizedBox(height: 10),
 
               //confirm password
-                MyTextField(
-                  controller: confirmPasswordController,
+                _buildInputField(
+                  icon: Icons.confirmation_num,
                   hintText: 'Confirm Password',
+                  controller: confirmPasswordController,
                   obscureText: true,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration( // Add some styling to input decoration
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: 'Confirm Password',
-                  ),
                 ),
 
 
-              const SizedBox(height: 25),
-                MyButton(
-                  //onTap: () => {signUserUp()}
-                  onTap: signUserUp,
-                  text: "Sign Up",
-                  color: Colors.purple,
-                  textColor: Colors.white,
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.purpleAccent),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    padding: MaterialStateProperty.all(
-                      EdgeInsets.symmetric(vertical: 16.0),
-                    ),
+              const SizedBox(height: 10),
+
+
+                              // Sign In Button
+                ElevatedButton(
+                  onPressed: signUserUp,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    shape: StadiumBorder(),
+                    elevation: 5,
                   ),
+                  child: Text('Sign In', style: TextStyle(color: Colors.white), ),
                 ),
+
               const SizedBox(height: 30),
 
               const Padding(
@@ -198,20 +163,20 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   children: [
                     Expanded(
                       child: Divider(
-                        color: Colors.black,
+                        color: Colors.white,
                         thickness: 0.5,
                       ),
                     ),
                     Padding(
                       padding: EdgeInsets.symmetric(horizontal: 10.0),
                       child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.black),
+                        'OR CONTINUE WITH',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
                     Expanded(
                       child: Divider(
-                        color: Colors.black,
+                        color: Colors.white,
                         thickness: 0.5,
                       ),
                     ),
@@ -223,25 +188,30 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
               // For google sign in
 
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                SquareTile(
-                    imagePath: 'lib/images/google.png',
-                    onTap: () => AuthService().signInGoogle()),
-              ]),
+               Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                 SquareTile(
+                     imagePath: 'lib/images/google.png',
+                     onTap: () => AuthService().signInGoogle()),
+               ]),
+              
+                // Google Sign-in Button
+              
 
               const SizedBox(height: 15),
 
               // end of UI, asks for registration
+
+
               Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 const Text('Already have an account? ',
-                    style: TextStyle(color: Colors.black)),
+                    style: TextStyle(color: Colors.white70)),
                 const SizedBox(width: 4),
                 GestureDetector(
                   onTap: widget.onTap,
                   child: const Text(
                     'Login Now',
                     style: TextStyle(
-                        color: Color.fromARGB(255, 47, 5, 65),
+                        color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
                 ),
@@ -249,6 +219,30 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ],
           ),
         )),
+      ),
+    );
+  }
+
+  Widget _buildInputField({
+    required IconData icon,
+    required String hintText,
+    required TextEditingController controller,
+    required bool obscureText,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.white70),
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.white70),
+        filled: true,
+        fillColor: Colors.white24,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
+        ),
       ),
     );
   }
